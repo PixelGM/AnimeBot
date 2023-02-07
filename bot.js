9e28643ab32ce1608aa81b95a39bd1eba238a309
ピクセル GM <35326620+PixelGM@users.noreply.github.com>
2023-02-06 23:52:08 -0800

    Initial commit

---
+++ bot.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const keepAlive = require("./server")
+
+client.on("ready", () => {
+    console.log("Strap the belt! I'm ready to go!");
+});
+
+
+client.on("message", message => {
+    if (message.author.bot) return;
+    // If user sends message Hi then reply with Hello
+    if (message.content === "Hi") {
+        message.reply("Hewwo :3");
+    }
+});
+
+keepAlive()
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
